2076a070423f088a69cf3e4d4417e3c061a19b4a
1000078 <sangho@onestorecorp.com>
2021-04-06 13:19:43 +0900

    init

---
+++ src/main/java/springboot/Application.java
@@ -0,0 +1,16 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main (String [] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+}
---
+++ src/main/java/springboot/HolomanConfig.java
@@ -0,0 +1,23 @@
+package springboot;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
+public class HolomanConfig {
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Holoman holoman(HolomanProperties properties) {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
+        return holoman;
+    }
+
+}
